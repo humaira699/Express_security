e90bfa2959fdbfa3e98f276b9b53768dc9225a6f
humaira699 <30345983+humaira699@users.noreply.github.com>
2021-11-29 18:31:33 +0500

    security

---
+++ models/userPassport.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+//registers a plugin for this schema
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
+
